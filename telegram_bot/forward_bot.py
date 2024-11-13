482ed65424af6bc67eac1f42de5aefe803e4300e
Json <JsonScript@duck.com>
2024-11-13 19:21:54 +0330

    Update forward_bot.py

--- telegram_bot/forward_bot.py
+++ telegram_bot/forward_bot.py
@@ -1,6 +1,6 @@
 import os
-from telegram import Bot, Update
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ContextTypes
 import logging
 
 # Set up logging
@@ -12,19 +12,16 @@ BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OWNER_ID = os.getenv("MY_CHAT_ID")
 
 # Create the bot instance
-updater = Updater(BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+app = Application.builder().token(BOT_TOKEN).build()
 
 # Define message handler function
-def forward_message(update: Update, context):
+async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
-    context.bot.send_message(chat_id=OWNER_ID, text=f"Message from {update.message.from_user.username}: {user_message}")
-    update.message.reply_text("Your message has been sent to the owner.")
+    await context.bot.send_message(chat_id=OWNER_ID, text=f"Message from {update.message.from_user.username}: {user_message}")
+    await update.message.reply_text("Your message has been sent to the owner.")
 
 # Add a message handler
-message_handler = MessageHandler(Filters.text & ~Filters.command, forward_message)
-dispatcher.add_handler(message_handler)
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message))
 
 # Start the bot
-updater.start_polling()
-updater.idle()
+app.run_polling()
