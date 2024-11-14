645f1217fe9ce0ce6715929fbeb4e126f4079e7b
Json <JsonScript@duck.com>
2024-11-14 18:08:58 +0330

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -2,8 +2,8 @@ import os
 import requests
 
 # Set your Telegram bot token and chat ID
-TELEGRAM_BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
-TELEGRAM_CHAT_ID = 'YOUR_CHAT_ID'
+TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 
 # Fetch the file contents
 with open('export.json', 'r') as f:
@@ -22,13 +22,13 @@ with open('lite', 'r') as f:
     lite_content = f.read()
 
 # Send the file contents as text messages to the Telegram bot
+def send_message(bot_token, chat_id, message):
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    data = {'chat_id': chat_id, 'text': message}
+    requests.post(url, data=data)
+
 send_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, "Export file content:\n\n" + export_content)
 send_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, "IPv4 file content:\n\n" + ipv4_content)
 send_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, "IPv6 file content:\n\n" + ipv6_content)
 send_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, "Full file content:\n\n" + full_content)
 send_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, "Lite file content:\n\n" + lite_content)
-
-def send_message(bot_token, chat_id, message):
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    data = {'chat_id': chat_id, 'text': message}
-    requests.post(url, data=data)
